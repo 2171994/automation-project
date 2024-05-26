6681406d4d6ee15eb25f4a666a28d41f77af0dd4
Omar Tarek <81044377+2171994@users.noreply.github.com>
2024-05-26 15:43:34 +0300

    Add files via upload

---
+++ src/test/java/driver_manager/DriverManager.java
@@ -0,0 +1,37 @@
+package driver_manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+
+import event_pages.login1_page;
+
+public class DriverManager {
+	
+	static login1_page log ;
+	    protected static WebDriver new_driver1;
+	    @BeforeSuite
+	    public static WebDriver getDriver() throws InterruptedException {
+	        if (new_driver1 == null) {
+	            // Set up the Chrome driver
+	        	System.setProperty("webdriver.chrome.driver", ".\\chrome4_driver\\chromedriver.exe");
+	        	 new_driver1  = new ChromeDriver();
+	       	  new_driver1 .manage().window().maximize();
+	       	  String baseurl = "https://dev5.ourpage.me/login" ;
+	         new_driver1 .get(baseurl) ;
+	     //  	new_driver1.navigate().to(baseurl);
+	       	  log = new login1_page(new_driver1);
+	       	  log.insert_email_field();
+	       	  log.insert_pass() ;
+	      	  log.click_On_signin();
+	        }
+	        return new_driver1;
+	    }
+
+	    public static void quitDriver() {
+	        if (new_driver1 != null) {
+	        	new_driver1.quit();
+	        	new_driver1 = null;
+	        }
+	    }
+	}
